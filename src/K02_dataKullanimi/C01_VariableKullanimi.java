e61376019dd62873b53b55e28085ef0c0b349869
GokcenS <pocchidesign@gmail.com>
2026-01-23 16:37:51 +0300

    deneme

---
+++ src/K02_dataKullanimi/C01_VariableKullanimi.java
@@ -0,0 +1,8 @@
+package K02_dataKullanimi;
+
+public class C01_VariableKullanimi {
+
+
+
+
+}
---
+++ src/K02_dataKullanimi/C02_DatasAndVariables.java
@@ -0,0 +1,22 @@
+package K02_dataKullanimi;
+
+public class C02_DatasAndVariables {
+    public static void main(String[] args) {
+        int sayi = 20 ;
+        System.out.println("Sayi degiskeninin degeri: "+ sayi);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
