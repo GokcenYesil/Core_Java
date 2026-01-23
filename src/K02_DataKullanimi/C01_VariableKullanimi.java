3fcb35ccc8721c5a5da6d3a06e50a60dd7a8265f
GokcenS <pocchidesign@gmail.com>
2026-01-23 17:07:32 +0300

    first commit

---
+++ src/K02_DataKullanimi/C01_VariableKullanimi.java
@@ -0,0 +1,31 @@
+package K02_DataKullanimi;
+
+public class C01_VariableKullanimi {
+    public static void main(String[] args) {
+        int sayi = 20;
+
+        System.out.println("hesabim");
+        System.out.println("sayi");
+        System.out.println("Java candir...");
+
+        System.out.println(sayi);
+
+        System.out.println("sayi variable'inin degeri : " + sayi);
+
+        int yas = 40;
+
+        System.out.println("yas variable'inin 30.satirdaki degeri : "  + yas);
+
+        yas = yas + 5;
+
+        System.out.println("yas variable'inin 35.satirdaki degeri : "  + yas);
+
+        yas = 2 * yas + 1;
+
+        System.out.println("yas variable'inin 42.satirdaki degeri : "  + yas);
+
+
+
+
+}
+}
