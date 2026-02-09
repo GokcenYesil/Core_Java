223b55927bf751ab83f3148ad0f5f237ca8c3cdc
GokcenS <gokcensafelyesil@gmail.com>
2026-02-10 00:54:20 +0300

    Week06_SampleJavaQuestions

---
+++ src/K39_Week06_SampleJavaQuestion/RG56_LinkedListteElemanlariTekTekYazdirma.java
@@ -0,0 +1,31 @@
+package K39_Week06_SampleJavaQuestion;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class RG56_LinkedListteElemanlariTekTekYazdirma {
+    public static void main(String[] args) {
+        /*56-----
+    Bir linkedList oluşturalım ve tüm elemanlarını tek tek yazdıralım.
+
+    LinkedList elemanları: sari,mavi,mor,yesil,beyaz
+
+    Beklenen Çıktı:
+    sari
+    mavi
+    mor
+    yesil
+    beyaz
+    */
+
+        LinkedList<String> lil = new LinkedList<>(Arrays.asList("sari", "mavi", "mor", "yesil", "beyaz"));
+
+        for (String each : lil) {
+            System.out.println(each);
+
+        }
+    }
+
+}
+
+
